fa2db69c2c133f859bf1b21b4072bf473589069e
Justin Pajela <justin@Justins-MacBook-Air-3.local>
2024-01-28 16:54:13 -0700

    123

--- src/stove/Burner.java
+++ src/stove/Burner.java
@@ -2,4 +2,19 @@ package stove;
 
 public class Burner {
 
+	public void plusButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void minusButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateTemperature() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
